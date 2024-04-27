4c5f587d8339155fef9134b3adad1404474133fa
Sam <samuelkoh17@gmail.com>
2024-04-27 15:02:04 +0800

    managed to get Twilio to send me a hello, there upon running dist/index.js, and installed express.js to allow node.js to be faster

--- dist/index.js
+++ dist/index.js
@@ -18,3 +18,18 @@ app.listen(PORT, () => {
     // gracefully handle error
     throw new Error(error.message);
 });
+
+// Twilio WhatsApp Business API sends custom message upon running
+// node dist/index
+//--------------------------------------------------------
+const accountSid = "AC25d72b09a5d2848d119c99ba66a8ba1f";
+const authToken = "a583d68657ac085e53bdf31ba87fc174";
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+      .create({
+         from: 'whatsapp:+14155238886',
+         body: 'Hello, there!',
+         to: 'whatsapp:+6586009948'
+       })
+      .then(message => console.log(message.sid));
