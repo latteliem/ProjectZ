33245728a81c66a70c51eb7a4b6027b0432fe57d
Sam <samuelkoh17@gmail.com>
2024-04-27 20:49:52 +0800

    Finished the development of bot with OpenAI API, but only left with user scanning the QR code (I dont know how to do that)

--- index.ts
+++ index.ts
@@ -17,3 +17,43 @@ app.listen(PORT, () => {
   // gracefully handle error
   throw new Error(error.message);
 });
+
+
+// Open AI API for WhatsApp
+//----------------------------------------------------------
+//import OpenAI from "openai";
+const {Client} = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
+const OpenAI = require("openai");
+require("dotenv").config();
+
+const client = new Client();
+
+client.on('qr',(qr) =>{
+    qrcode.generate(qr,{small:true});
+});
+
+client.on('ready',() =>{
+    console.log("Client is ready");
+});
+
+client.initialize();
+
+const configuration = new OpenAI({
+    apiKey : process.env.SECRET_KEY,
+});
+const openai = new OpenAI(configuration);
+
+async function runCompletion(message){
+    const completion = await openai.createCompletion({
+        model:"text-davinci-003",
+        prompt: message,
+        max_tokens: 200,
+    });
+    return completion.data.choices[0].text;
+}
+
+client.on('message',message => {
+    console.log(message.body);
+    runCompletion(message.body).then(result => message.reply(result));
+})
