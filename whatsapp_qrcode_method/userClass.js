7a0c70cefa71296501072ca2732864d64f3f6ad0
Jovin2301 <s10243666@connect.np.edu.sg>
2024-05-17 02:09:19 +0800

    Adding and connecting to stripe api within vscode.
    creating a check out session for the user and then retrive the user shopping cart, and check out for those products within the shopping cart.
    
    addToCart function has been edited within the userClass.js to better formula the entire process of billing out.

--- whatsapp_qrcode_method/userClass.js
+++ whatsapp_qrcode_method/userClass.js
@@ -6,14 +6,15 @@ class User {
         this.shoppingCart = [];
     };
 
-    addToCart(prodID, prodName, prodSellPrice, prodQuantity){
-        const productInCart = { //creating a function to add the product and the quantity of product that the user would like to purchase
-            prodid: prodID,
-            prodname: prodName,
-            prodsellprice: prodSellPrice,
-            prodQuantity: prodQuantity
-        };
-        this.shoppingCart.push(productInCart);    //adds the product that the user wants to buy to the empty shopping cart array    
+     //creating a function to add the product and the quantity of product that the user would like to purchase
+    addToCart(product, prodQuantity){
+        if (prodQuantity > product.prodquan){
+            console.log('You are purchasing more than what is available in store. Please click in another quantity to purchase!');
+            return; // Exit the function if the quantity is not available
+        }
+        
+        this.shoppingCart.push(product);    //adds the product that the user wants to buy to the empty shopping cart array   
+        console.log('Product has been added to cart');
     }
 
     paymentCheck(paymentID, paymentStatus){
