112032a82e8f98649d34c108f78028d6346f2477
Sam <samuelkoh17@gmail.com>
2024-05-16 21:08:45 +0800

    now trying to log all messages upon using express application to the POST /whatsapp endpoint

--- another_tut/src/index.js
+++ another_tut/src/index.js
@@ -1,7 +1,8 @@
 // external packages
 const express = require('express');
 const bodyParser = require('body-parser');
-require('dotenv').config();
+//const whatsappSendMessage = require('../helper-function/whatsapp-send-message.js');
+//re('dotenv').config();
 
 // Start the webapp
 const webApp = express();
@@ -20,20 +21,16 @@ webApp.get('/', (req, res) => {
     res.send(`Hello World.!`);
 });
 
-const WA = require('../helper-function/whatsapp-send-message');
+//const WA = require('../helper-function/whatsapp-send-message');
 
 // Route for WhatsApp
 webApp.post('/whatsapp', async (req, res) => {
-
+    console.log(req.body);
     let message = req.body.Body;
     let senderID = req.body.From;
 
     console.log(message);
-    console.log(senderID);
-
-    // Write a function to send message back to WhatsApp
-    await WA.sendMessage('Hello from the other side.', senderID);
-
+    console.log(senderID)
 });
 
 // Start the server
