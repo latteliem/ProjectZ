6883d3a5756b8215a79796739213f23aee662018
Sam <samuelkoh17@gmail.com>
2024-05-16 21:17:43 +0800

    did try and catch error handling of sending whatsapp message

--- another_tut/helper-function/whatsapp-send-message.js
+++ another_tut/helper-function/whatsapp-send-message.js
@@ -5,3 +5,18 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken, {
   lazyLoading: false,
 });
+
+// funciton to send message to WhatsApp
+const sendMessage = async(message, senderID) => {
+
+    try {
+        await client.messages.create({
+            to: senderID,
+            body: message,
+            from: "whatsapp +14155238886"
+        });
+    } catch(error) {
+        console.log("Error at send message --> ${error} ")
+    }
+};
+
