66f242479204b4e9b9219d0b9068f21a4ef12909
Sam <samuelkoh17@gmail.com>
2024-05-26 22:15:28 +0800

    implemented a databaseHelper js file to refactor the code for mongodb, then separated all to smaller functions as we have more than 3 "if" statements

---
+++ another_tut/src/databaseHelper.js
@@ -0,0 +1,60 @@
+const { MongoClient } = require('mongodb');
+
+const connectToDatabase = () => {
+    const uri = process.env.MONGODB_URI;
+    const client = new MongoClient(uri);
+    const dbName = 'LumiChat_database';
+
+    return client.connect().then(() => {
+        const db = client.db(dbName);
+        console.log('Connected to database');
+        return db;
+    }).catch(err => {
+        console.error(err);
+        throw err;
+    });
+};
+
+const findUserByUsername = (db, username) => {
+    const userCollection = db.collection('userCollection');
+    return userCollection.findOne({ username }).catch(err => {
+        console.error('Error finding user:', err);
+        throw err;
+    });
+};
+
+const findProductById = (db, productId) => {
+    const productCollection = db.collection('productCollection');
+    return productCollection.findOne({ prodid: productId }).catch(err => {
+        console.error('Error finding product:', err);
+        throw err;
+    });
+};
+
+const addToCart = async (user, product, quantityToPurchase) => {
+    const productInCart = {
+        prodid: product.prodid,
+        prodname: product.prodname,
+        prodsprice: product.prodsprice,
+        quantityToPurchase
+    };
+    user.shoppingCart.push(productInCart);
+};
+
+const updateUserCart = (userCollection, user) => {
+    return userCollection.updateOne(
+        { userid: user.userid },
+        { $set: { shoppingCart: user.shoppingCart } }
+    ).catch(err => {
+        console.error('Error updating cart:', err);
+        throw err;
+    });
+};
+
+module.exports = {
+    connectToDatabase,
+    findUserByUsername,
+    findProductById,
+    addToCart,
+    updateUserCart
+};
